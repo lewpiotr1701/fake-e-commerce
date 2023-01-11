90d78e5d36f47b7f6530c8a6a9339ee569628391
Piotr <lewczykpiotr@gmail.com>
2023-01-11 14:29:17 +0100

    add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
+  <v-app>
+    <v-app-bar title="Application"></v-app-bar>
 
+    <v-navigation-drawer>...</v-navigation-drawer>
+
+    <v-main>...</v-main>
+  </v-app>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,19 @@ import store from './store/index.js'
 
 import './assets/main.css'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 const app = createApp(App)
 
-app.use(router).use(store)
+app.use(router).use(store).use(vuetify)
 
 app.mount('#app')
