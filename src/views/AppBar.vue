f62bae78be780f04f2427bd6dc2b70622b281440
Piotr <lewczykpiotr@gmail.com>
2023-01-19 23:11:48 +0100

    paste NavigationDrawer into AppBar

--- src/views/AppBar.vue
+++ src/views/AppBar.vue
@@ -13,7 +13,25 @@
     <v-btn variant="text" icon="mdi-account"></v-btn>
   </v-app-bar>
 
-  <NavigationDrawer :isDrawer="drawer" />
+  <v-navigation-drawer v-model="drawer" location="left" temporary>
+    <v-list>
+
+      <router-link :to="{ name: 'Homepage' }">
+        <v-list-item prepend-icon="mdi-home" title="Home" link></v-list-item>
+      </router-link>
+
+      <router-link :to="{ name: 'About' }">
+        <v-list-item prepend-icon="mdi-information" title="About" link></v-list-item>
+      </router-link>
+
+      <v-divider></v-divider>
+
+      <router-link :to="{ name: 'ProductList' }">
+        <v-list-item prepend-icon="mdi-view-list" title="Products" link></v-list-item>
+      </router-link>
+
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
@@ -42,5 +60,8 @@ export default {
 </script>
 
 <style scoped>
-
+a {
+  text-decoration: none;
+  color: inherit;
+}
 </style>
